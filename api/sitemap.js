8446c0f63a066765f8a97b9369ad910c49e0fb6b
sopsofiah15-cloud <sopsofiah15@gmail.com>
2026-07-11 18:39:47 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,14 +1,22 @@
 const { getSql } = require('../lib/db');
 
+function escapeXml(str = '') {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
 module.exports = async (req, res) => {
   try {
     const sql = getSql();
     const rows = await sql`
-      SELECT slug, updated_at
+      SELECT slug, updated_at, thumbnail, judul
       FROM articles
       ORDER BY updated_at DESC
     `;
-
     const host = req.headers['x-forwarded-host'] || req.headers.host;
     const protocol = req.headers['x-forwarded-proto'] || 'https';
     const baseUrl = `${protocol}://${host}`;
@@ -18,16 +26,27 @@ module.exports = async (req, res) => {
       ...rows.map((r) => ({
         loc: `${baseUrl}/berita/${r.slug}`,
         lastmod: new Date(r.updated_at || Date.now()).toISOString(),
+        image: r.thumbnail,
+        title: r.judul,
       })),
     ];
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${urls
   .map(
     (u) => `  <url>
     <loc>${u.loc}</loc>
-    <lastmod>${u.lastmod}</lastmod>
+    <lastmod>${u.lastmod}</lastmod>${
+      u.image
+        ? `
+    <image:image>
+      <image:loc>${escapeXml(u.image)}</image:loc>
+      <image:title>${escapeXml(u.title || '')}</image:title>
+    </image:image>`
+        : ''
+    }
   </url>`
   )
   .join('\n')}
